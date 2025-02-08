e57cc3ed3810d066e67b7b0b61f62064f6afc148
yhw <yhw1150787158@163.com>
2025-02-08 21:10:32 +0800

    日常练习

--- point/main.go
+++ point/main.go
@@ -1,11 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+func modify(slice []int) {
+	slice[0] = 100
+	slice = append(slice, 200)
+	fmt.Println(slice)
+}
+
+func checkType(T interface{}) {
+	switch T.(type) {
+	case int:
+		fmt.Println("int")
+	case float64:
+		fmt.Println("float64")
+	case string:
+		fmt.Println("string")
+	default:
+		fmt.Println("not a type")
+	}
+
+}
 
 func main() {
+	s := []int{1, 2, 3}
+	modify(s)
+	fmt.Println(s)
+	checkType(s)
+	ch := make(chan int, 2)
+	//go func() {
+	ch <- 1
+	ch <- 2
+	close(ch)
+	//}()
 
-	var p *int
-	a := 2
-	p = &a
-	fmt.Println(p)
+	time.Sleep(time.Second)
+	for v := range ch {
+		fmt.Print(v, " ")
+	}
+
 }
