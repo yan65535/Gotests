6e81408377766ee393672972a7838626a22462be
yhw <yhw1150787158@163.com>
2025-04-03 16:54:49 +0800

    grpcdemo

--- grpc-demo/client/main.go
+++ grpc-demo/client/main.go
@@ -1,1 +1,34 @@
-package client
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"time"
+
+	"google.golang.org/grpc"
+	pb "grpc-demo/proto/protoFiles/hello" // 替换为实际的包路径
+)
+
+func main() {
+	// 连接到服务端
+	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewHelloServiceClient(conn)
+
+	// 准备请求
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	name := "yhw"
+	if len(os.Args) > 1 {
+		name = os.Args[1]
+	}
+	r, err := c.SayHello(ctx, &pb.HelloRequest{Name: name})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	log.Printf("Greeting: %s", r.Message)
+}
--- grpc-demo/server/main.go
+++ grpc-demo/server/main.go
@@ -1,1 +1,33 @@
-package server
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+	pb "grpc-demo/proto/protoFiles/hello" // 替换为实际的包路径
+)
+
+// 实现 HelloService 接口
+type server struct {
+	pb.UnimplementedHelloServiceServer
+}
+
+// 实现 SayHello 方法
+func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloResponse, error) {
+	return &pb.HelloResponse{Message: "Helloss, " + in.Name}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterHelloServiceServer(s, &server{})
+	log.Printf("server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
