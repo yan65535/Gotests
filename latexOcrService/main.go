0fd1ae3ed1f82a66a5b1ad32366eb8fe0005b86f
yhw <yhw1150787158@163.com>
2025-04-10 21:05:33 +0800

    更新了zookeeper服务组件

--- latexOcrService/main.go
+++ latexOcrService/main.go
@@ -1,6 +1,7 @@
 package latexOcrService
 
 import (
+	"github.com/samuel/go-zookeeper/zk"
 	"github.com/zeromicro/go-zero/core/discov"
 	"github.com/zeromicro/go-zero/core/service"
 	"github.com/zeromicro/go-zero/zrpc"
@@ -10,6 +11,7 @@ import (
 	"latexOcrService/config"
 	latexOcr "latexOcrService/latexOcrProto"
 	"latexOcrService/rpc"
+	"time"
 )
 
 //tkRpc "e.coding.net/g-mneg1542/block/block.proto/token/protoc/token"
@@ -18,6 +20,19 @@ import (
 
 func main() {
 	conf := initConfig()
+
+	// 初始化Zookeeper连接
+	zkConn := initZookeeper(conf)
+	defer zkConn.Close()
+
+	// 初始化gRPC服务器
+	grpcServer := initGrpcServer(conf, adaptor.Adaptors{})
+
+	// 注册服务到Zookeeper
+	registerToZookeeper(zkConn, conf.Zookeeper.ServicePath, conf.Server.RpcConf.ListenOn)
+
+	// 启动服务
+	grpcServer.Start()
 
 }
 
@@ -67,3 +82,33 @@ func initGrpcServer(conf config.Conf, adaptor adaptor.Adaptors) *zrpc.RpcServer 
 //	app := uweb.NewApp(conf.Server.HttpPort, "", tracer, router)
 //	return app
 //}
+
+// 初始化Zookeeper连接
+func initZookeeper(conf config.Conf) *zk.Conn {
+	conn, _, err := zk.Connect(
+		conf.Zookeeper.Hosts,
+		time.Duration(conf.Zookeeper.Timeout)*time.Second,
+	)
+	handleErr(err)
+	return conn
+}
+
+// Zookeeper服务注册
+func registerToZookeeper(conn *zk.Conn, servicePath string, listenOn string) {
+	// 创建持久节点（如果不存在）
+	exists, _, err := conn.Exists(servicePath)
+	handleErr(err)
+
+	if !exists {
+		_, err = conn.Create(servicePath, []byte{}, 0, zk.WorldACL(zk.PermAll))
+		handleErr(err)
+	}
+
+	// 创建临时顺序节点
+	nodePath := servicePath + "/node-"
+	_, err = conn.CreateProtectedEphemeralSequential(
+		nodePath,
+		[]byte(listenOn),
+		zk.WorldACL(zk.PermAll))
+	handleErr(err)
+}
